4475285fad4d85fa0a14a862ae3a0a7fce884545
André Luiz <50808997+aluizgc@users.noreply.github.com>
2019-11-30 14:41:40 -0300

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,193 @@
+'''
+Artificial Bee Colony Algorithm
+
+Author: aluizgc
+'''
+
+import numpy as np
+from scipy.constants import e, Boltzmann
+
+np.seterr(divide='ignore', invalid='ignore')
+np.set_printoptions(precision=4)
+
+#-- Variables --#
+
+qE = e  # electron charge (C)
+boltzmann = Boltzmann  # Bolztmann constant (J/K)
+temperature = 33. + 273.15
+maxIter = 20000
+currIter = 0
+'''data = np.loadtxt('BHCS/curva_0.txt')
+voltage = data[:, 0]
+current = data[:, 1]'''
+lbound = np.array([0.0, 0.0, 0.0, 0.0, 1.0])
+ubound = np.array([1.0, 1e-6, 0.5, 100.0, 2.0])
+# bound = [Iph (A), Isd (muA), Rs (ohm), Rsh (ohm), n (diode ideality factor) ]
+
+m = len(lbound)  # number of variables
+SN = 100  # populaton size
+limit = 0.06*maxIter  # abandonment limit
+runs = 10
+results = np.zeros((runs,m))
+costs = np.zeros((runs,40))
+
+#-- Cost function --#
+
+def costFunc(x):
+    ISim = x[0]-x[1]*(np.exp(qE*(voltage+current*x[2])/(
+        x[4]*boltzmann*temperature), dtype=np.float64)-1)-(voltage+current*x[2])/x[3]
+    return np.power((1/np.float(np.size(voltage)))*np.sum(np.power(ISim-current, 2, dtype=np.float64), dtype=np.float64), 0.5, dtype=np.float64)
+
+#-- Fitness function --#
+
+def fitness(foodSource):
+    costF = np.zeros((SN, 1))
+    fit = np.zeros((SN, 1))
+    for i in range(SN):
+        costF[i] = costFunc(foodSource[i, :])
+        if costF[i] >= 0:
+            fit[i] = 1./(1.+costF[i])
+        if costF[i] < 0:
+            fit[i] = 1.+np.abs(costF[i])
+    return fit
+
+#-- Probability and Roulette Wheel functions --#
+
+def prob(fit, foodSource):
+    r = np.random.uniform(0., 1.)
+    pn = np.zeros(SN)
+    chosen = np.zeros((SN, m))
+    for i in range(SN):
+        pn[i] = fit[i]/np.sum(fit)
+        if r <= pn[i]:
+            chosen[i] = foodSource[i,:]
+    return chosen
+
+#-- Penalty Function --#
+def Penalty(sol):
+    for j in range(m):
+        if sol[j] > ubound[j]:
+            sol[j] = lbound[j]+np.random.random()*(ubound[j]-lbound[j])
+        if sol[j] < lbound[j]:
+            sol[j] = lbound[j]+np.random.random()*(ubound[j]-lbound[j])
+    return sol
+    
+###--- ABC MAIN LOOP ---###
+
+#-- Initialization phase --#
+# Initialize random food source
+for run in range(runs):
+    data = np.loadtxt('curva0.txt')
+    voltage = data[:, 0]
+    current = data[:, 1]
+    Xn = np.zeros((SN, m))  # initial solution matrix
+    Vn = np.zeros((SN, m))  # new solutions matrix
+    bas = np.zeros(SN)  # abandonment counter
+    k = 0
+    costXn = np.zeros(SN)
+    costVn = np.zeros(SN)
+    olbXn = np.zeros((SN,m))
+    olbVn = np.zeros((SN,m))
+    costOLXn = np.zeros(SN)
+    costOLVn = np.zeros(SN)
+    olbXnIndex = np.zeros(SN)
+    bestCost = 0
+    index = 0
+    bestSolution = np.zeros((m))
+    
+    for i in range(SN):
+        for j in range(m):
+            Xn[i][j] = lbound[j]+np.random.random()*(ubound[j]-lbound[j])
+        costXn[i] = costFunc(Xn[i,:])
+    index = np.argmin(costXn)
+    bestSolution = Xn[index,:]
+        
+    for currIter in range(1,maxIter+1):
+    #-- Employed Bees Phase --#
+    # Initialize a second food source based on the first one.
+    # Do greedy selection and fitness calculation.
+        for i in range(SN):
+            for j in range(m):
+                if (k == i) or (k == 0):
+                    while (k == i) or (k == 0):
+                        k = np.random.randint(SN)
+                Vn[i][j] = Xn[i][j] + np.random.uniform()*(Xn[i][j]-Xn[k][j])
+                k = 0
+            Vn[i,:] = Penalty(Vn[i,:])
+            costVn[i] = costFunc(Vn[i,:])
+            if costVn[i] < costXn[i]:
+                Xn[i,:] = Vn[i,:]
+                bas[i] = 0
+            else:
+                bas[i] = bas[i]+1
+            Xn[i,:] = Penalty(Xn[i,:])
+            costXn[i] = costFunc(Xn[i,:])        
+
+        fit = fitness(Xn)
+        
+        #-- Onlooker Bees Phase --#
+        # Probabilities and roullete selection
+        # Food source for onlooker bees (Xn)
+        while not olbXn.any():
+            olbXn = prob(fit, Xn)
+
+        # Not zeros index for olbXn
+        for i in range(SN):
+            if np.all(olbXn[i,:] != 0):
+                olbXnIndex[i] = i
+        for t in range(SN):
+            for j in range(m):
+                if (k == olbXnIndex[t]) or (k == 0):
+                    while (k == olbXnIndex[t]) or (k == 0):
+                        k = np.random.randint(SN)
+                if t == olbXnIndex[t]:
+                    if olbXnIndex[t] == 0:
+                        olbVn[t,:] = olbXn[t,:]
+                    else:
+                        olbVn[t][j] = olbXn[t][j] + np.random.uniform() * (olbXn[t][j]-olbXn[k][j])
+                k = 0
+            costOLXn[t] = costFunc(olbXn[t,:])
+            costOLVn[t] = costFunc(olbVn[t,:])
+            if  costOLVn[t] < costOLXn[t]:
+                olbXn[t,:] = olbVn[t,:]
+            olbXn[t,:] = Penalty(olbXn[t,:])
+        
+        # Comparision
+        for i in range(SN):
+            if costOLXn[i] < costXn[i]:
+                Xn[i,:] = olbXn[i,:]
+                bas[i] = 0
+            else:
+                bas[i] = bas[i]+1
+            Xn[i,:] = Penalty(Xn[i,:])
+        bestCost = costFunc(bestSolution)
+        #-- Update best solution --#
+        for i in range(SN):
+            if costXn[i] <= bestCost:
+                bestSolution = Xn[i,:]
+                bas[i] = 0
+        bestCost = costFunc(bestSolution)
+        
+        #-- Scout bees phase --#
+        for i in range(SN):
+            if bas[i] >= limit:
+                for j in range(m):
+                    Xn[i][j] = lbound[j]+np.random.random()*(ubound[j]-lbound[j])
+                    bas[i] = 0
+            Xn[i,:] = Penalty(Xn[i,:])
+        
+        if currIter%500 == 0 :
+            costs[run][int(currIter/500)-1] = bestCost
+        
+        
+        #-- Print result for each iteration
+        print('Run: ', run)
+        print('It: ',currIter)
+        print('%.4E' %bestCost, bestSolution)
+    results[run] = bestSolution
+            
+    np.savetxt('ABCrun{}.txt'.format(run+1), bestSolution, delimiter= ' ')
+    np.savetxt('ABCcosts{}.txt'.format(run+1), costs)    
+
+
+
---
+++ bhcs.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 10 19:54:19 2019
+
+@author: adhimarflavio
+"""
+import numpy as np
+from scipy.special import gamma
+
+# Parameter settings for the SCBH
+NP = 100  # population size
+pa = 0.3  # discovery probability
+alpha = 1.1  # alpha> 0  is a parameter determining the step size of cuckooo
+beta = 1.7  # is Levy flight exponent
+delta = 1.6
+I = 1.   # maximum imigration
+E = 1.   # imigration rate
+maxFES = 2000
+#FES = 0
+runs = 10
+#run = 0
+
+
+
+lbound = np.array([0.0, 0.0, 0.0, 1.0, 0.0])  # I_ph I_0 R_s n R_sh
+ubound = np.array([1.0, 1e-6, 0.5, 2.0, 100.0])
+dimension = len(lbound)
+# Compute migration rates, assuming the population is sorted from most fit to least fit
+mu = E*((NP-(np.arange(NP)+1.))/NP)  # emigration rate
+lambdaa = I*(1. - mu)  # immigration rate
+# --- Define constants and import data --- #
+
+charge = 1.6021766e-19
+boltz = 1.38065e-23
+temperature = 33 + 273.15  # in Kelvin
+
+#data = np.loadtxt('BHCS Adhimar/curva_0.txt')
+#voltage = data[:, 0]
+#current = data[:, 1]
+
+# --- Define objective function (RMSE) --- #
+
+
+def objective(indv):
+    simulated = indv[0]-indv[1]*(np.exp(charge*(voltage+current*indv[2])/(
+        indv[3]*boltz*temperature), dtype=np.float64)-1)-(voltage+current*indv[2])/indv[4]
+    return np.power((1/np.float(np.size(voltage)))*np.sum(np.power(current-simulated, 2, dtype=np.float64), dtype=np.float64), 0.5, dtype=np.float64)
+# --- Penalty function --- #
+
+
+def Penalty(sol):
+    for j in range(dimension):
+        if sol[j] > ubound[j]:
+            sol[j] = lbound[j] + np.random.rand()*(ubound[j]-lbound[j])
+        if sol[j] < lbound[j]:
+            sol[j] = lbound[j] + np.random.rand()*(ubound[j]-lbound[j])
+    return sol
+results = np.zeros((runs,dimension))
+costs = np.zeros((runs,40))
+for run in range(runs):
+    #data = np.loadtxt('Melhores medidas/{}.txt'.format(run))
+    data = np.loadtxt('curva_0.txt')
+    voltage = data[:, 0]
+    current = data[:, 1]
+    #results = np.zeros((runs,dimension))
+    #costs = np.zeros((runs,50))
+    # Random initial solutions
+    nest = lbound+np.random.rand(NP, dimension)*(ubound-lbound)
+    fit_nest = np.zeros(NP)
+    for i in range(0, NP):
+        fit_nest[i] = objective(nest[i, :])
+    fmin, index = np.min(fit_nest), np.argmin(fit_nest)
+    bNest = nest[index, :]
+
+    nest_new = nest.copy()
+    fit_nest_new = fit_nest.copy()
+
+    for FES in range(1,maxFES+1):
+        sigma_u = (gamma(1.+beta)*np.sin(np.pi*beta/2.) /
+                (gamma((1.+beta)/2.)*beta*2.**((beta-1.)/2.)))**(1./beta)
+        sigma_v = 1.
+        nest_mean = np.mean(nest)
+        for i in range(0, NP):
+            u = np.random.randn(dimension)*sigma_u
+            v = np.random.randn(dimension)*sigma_v
+            sr = np.random.rand()
+            if (sr > 2./3.):
+                s = nest[i, :]+alpha * \
+                    ((0.01*u)/(np.abs(v)**(1./beta)))*(nest[i, :] - bNest[:])
+            elif ((sr <= 2./3.) and (sr > 1./3.)):
+                s = nest_mean + delta * \
+                    np.log(1./np.random.rand())*(nest_mean - nest[i, :])
+            else:
+                s = nest[i, :]+delta*np.exp(np.random.rand())*(bNest-nest[i, :])
+            nest_new[i, :] = Penalty(s)
+            fit_nest_new[i] = objective(nest_new[i, :])
+    # if better than
+        for i in range(0, NP):
+            if (fit_nest_new[i] < fit_nest[i]):
+                fit_nest[i] = fit_nest_new[i]
+                nest[i, :] = nest_new[i, :]
+        fmin, index = np.min(fit_nest), np.argmin(fit_nest)
+        bNest = nest[index, :]
+    # Biogeography-based discovery operator
+        sort = np.argsort(fit_nest)
+        fit_nest = fit_nest[sort]
+        nest = nest[sort]
+        for i in range(0, NP):
+            for j in range(0, dimension):
+                if (np.random.rand() > pa):
+                    pp = mu/np.sum(mu)
+                    cum_pp = np.cumsum(pp)
+                    SelectIndex = np.where(np.random.rand() < cum_pp)
+                    # print("SelectIndex",SelectIndex[0][0])
+                    while (SelectIndex[0][0] == i):
+                        SelectIndex = np.where(np.random.rand() < cum_pp)
+                    Alpha = alpha  # np.random.rand()
+                    # migration step
+                    nest_new[i, j] = Alpha*nest[i, j] + \
+                        (1.-Alpha)*nest[SelectIndex[0][0], j]
+                else:
+                    nest_new[i, j] = nest[i, j]
+        # Selection
+        for i in range(0, NP):
+            nest_new[i, :] = Penalty(nest_new[i, :])
+            fit_nest_new[i] = objective(nest_new[i, :])
+            if (fit_nest_new[i] < fit_nest[i]):
+                # print("ok")
+                fit_nest[i] = fit_nest_new[i]
+                nest[i, :] = nest_new[i, :]
+        fmin, index = np.min(fit_nest), np.argmin(fit_nest)
+        bNest = nest[index, :]
+        if FES%50 == 0:
+            costs[run][int(FES/50)-1] = fit_nest[0]
+        print('Run: ', run)
+        print('It: ', FES)
+        print('%.4E' %fit_nest[0], nest[0, :])
+        
+    results[run] = nest[0,:]
+    np.savetxt('BHCSrun.txt', results, delimiter= ' ')
+    np.savetxt('BHCScosts.txt',costs)    
+
+
